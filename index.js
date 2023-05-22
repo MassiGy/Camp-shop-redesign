fd2820f64c70a7039fe9b05b996025b047a62ff4
MassiGy <ghernaoutmassi@gmail.com>
2023-05-23 00:42:16 +0200

    +master : trying to fix the cookies on production

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const sessionConfig = {
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV == 'production',
+        secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
     },
     store: MongoStore.create({
